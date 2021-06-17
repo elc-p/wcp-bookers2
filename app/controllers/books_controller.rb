9c2e72a2d835ed0746a1a92bcd6b8a573866a2ea
elc-p <yomtype10@gmail.com>
2021-06-17 12:24:24 +0000

    [Add]link

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,7 +26,7 @@ class BooksController < ApplicationController
   
   def show
     @book = Book.find(params[:id])
-    @user = User.find(@book.user_id)
+    @user = User.find(@book.user.id)
   end
   
   def edit
